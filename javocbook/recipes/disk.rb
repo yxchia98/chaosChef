4e4f7190d923b9b16aa706fdee3fd2f372eb5a76
Lulmao <yxchia98@gmail.com>
2021-08-13 00:57:26 +0800

    updated databag to contain validations

---
+++ javocbook/recipes/disk.rb
@@ -0,0 +1,36 @@
+bagitem = data_bag_item('javoc_data_bag', 'disk')
+bagitem['utilization_level'].each do |n|
+	if platform?('redhat')
+		execute "Disk Load at #{n}% for #{bagitem['duration'].at(1)}mins (redhat)" do
+			command <<-EOS
+			cd #{ENV['HOME']}
+			#{bagitem['jrepath_linux']} #{bagitem['jvm_parameters']} Javoc.jar #{"-" + bagitem['type']} #{bagitem['duration'].at(1)} #{n} #{bagitem['validate_service_linux'] == "" ? "" : "-service " + bagitem['validate_service_linux']} #{bagitem['validate_url'] == "" ? "" : "-url " + bagitem['validate_url']} -seconds
+			EOS
+		end
+		log "after test log" do
+			message "Finished executing Disk Load at #{n}% for #{bagitem['duration'].at(2)}mins"
+			level :info
+		end
+		chef_sleep 'pause inbetween experiments' do
+			seconds 10
+			action :sleep
+		end
+	elsif platform?('windows')
+		powershell_script "Disk Load at #{n}% for #{bagitem['duration'].at(1)}mins (windows)" do
+			code <<-EOS
+			cd #{ENV['HOME']}
+			#{bagitem['jrepath_windows']} #{bagitem['jvm_parameters']} Javoc.jar #{"-" + bagitem['type']} #{bagitem['duration'].at(1)} #{n} #{bagitem['validate_service_windows'] == "" ? "" : "-service " + bagitem['validate_service_windows']} #{bagitem['validate_url'] == "" ? "" : "-url " + bagitem['validate_url']} -seconds
+			EOS
+		end
+		log "after test log" do
+			message "Finished executing Disk Load at #{n}% for #{bagitem['duration'].at(2)}mins"
+			level :info
+		end
+		chef_sleep 'pause inbetween experiments' do
+			seconds 10
+			action :sleep
+		end
+	else
+
+	end
+end
---
+++ javocbook/recipes/netdrop.rb
@@ -0,0 +1,36 @@
+bagitem = data_bag_item('javoc_data_bag', 'netdrop')
+bagitem['utilization_level'].each do |n|
+	if platform?('redhat')
+		execute "Network packet loss at #{n}% for #{bagitem['duration'].at(0)}mins (redhat)" do
+			command <<-EOS
+			cd #{ENV['HOME']}
+			#{bagitem['jrepath_linux']} #{bagitem['jvm_parameters']} Javoc.jar #{"-" + bagitem['type']} #{bagitem['duration'].at(0)} #{n} #{bagitem['validate_service_linux'] == "" ? "" : "-service " + bagitem['validate_service_linux']} #{bagitem['validate_url'] == "" ? "" : "-url " + bagitem['validate_url']} -seconds
+			EOS
+		end
+		log "after test log" do
+			message "Finished executing Network packet loss at #{n}% for #{bagitem['duration'].at(0)}mins"
+			level :info
+		end
+		chef_sleep 'pause inbetween experiments' do
+			seconds 10
+			action :sleep
+		end
+	elsif platform?('windows')
+		powershell_script "Network packet loss at #{n}% for #{bagitem['duration'].at(0)}mins (windows)" do
+			code <<-EOS
+			cd #{ENV['HOME']}
+			#{bagitem['jrepath_windows']} #{bagitem['jvm_parameters']} Javoc.jar #{"-" + bagitem['type']} #{bagitem['duration'].at(0)} #{n} #{bagitem['validate_service_windows'] == "" ? "" : "-service " + bagitem['validate_service_windows']} #{bagitem['validate_url'] == "" ? "" : "-url " + bagitem['validate_url']} -seconds
+			EOS
+		end
+		log "after test log" do
+			message "Finished executing Network packet loss at #{n}% for #{bagitem['duration'].at(0)}mins"
+			level :info
+		end
+		chef_sleep 'pause inbetween experiments' do
+			seconds 10
+			action :sleep
+		end
+	else
+
+	end
+end
