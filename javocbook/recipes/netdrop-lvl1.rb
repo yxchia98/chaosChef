5a4b416a35c95c04946dfd94b2cbdc36628d7193
Lulmao <yxchia98@gmail.com>
2021-08-16 11:39:33 +0800

    update recipe files

--- javocbook/recipes/netdrop-lvl1.rb
+++ javocbook/recipes/netdrop-lvl1.rb
@@ -1,10 +1,11 @@
 bagitem = data_bag_item('javoc_data_bag', 'netdrop')
+validator = data_bag_item('javoc_data_bag', 'validator')
 bagitem['duration'].each do |n|
 	if platform?('redhat')
 		execute "Network Packet Loss at #{bagitem['utilization_level'].at(0)}% for #{n}mins (redhat)" do
 			command <<-EOS
 			cd #{ENV['HOME']}
-			#{bagitem['jrepath_linux']} #{bagitem['jvm_parameters']} Javoc.jar -#{bagitem['type']} #{n} #{bagitem['utilization_level'].at(0)} -seconds
+			#{bagitem['jrepath_linux']} #{bagitem['jvm_parameters']} Javoc.jar -#{bagitem['type']} #{n} #{bagitem['utilization_level'].at(0)} #{validator['validate_service_linux'] == "" ? "" : "-service " + validator['validate_service_linux']} #{validator['validate_url'] == "" ? "" : "-url " + validator['validate_url']} -seconds
 			EOS
 		end
 		log "after test log" do
@@ -19,7 +20,7 @@ bagitem['duration'].each do |n|
 		powershell_script "Network Packet Loss at #{bagitem['utilization_level'].at(0)}% for #{n}mins (windows)" do
 			code <<-EOS
 			cd #{ENV['HOME']}
-			#{bagitem['jrepath_windows']} #{bagitem['jvm_parameters']} Javoc.jar -#{bagitem['type']} #{n} #{bagitem['utilization_level'].at(0)} -seconds
+			#{bagitem['jrepath_windows']} #{bagitem['jvm_parameters']} Javoc.jar -#{bagitem['type']} #{n} #{bagitem['utilization_level'].at(0)} #{validator['validate_service_windows'] == "" ? "" : "-service " + validator['validate_service_windows']} #{validator['validate_url'] == "" ? "" : "-url " + validator['validate_url']} -seconds
 			EOS
 		end
 		log "after test log" do
